4134c1c54730c4aa52cf55d0e1467313da7d325b
qingwen.yan <qingwen.yan@dji.com>
2016-07-03 17:49:53 +0800

    update sh

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,33 @@
-var gulp = require('gulp'),
-    ossup = require('gulp-oss-up'),
-    newer = require('gulp-newer'),
-    useref = require('gulp-useref'),
-    clean = require('gulp-clean'),
-    exec = require('child_process').exec,
-    runSequence = require('run-sequence'),
-    rev = require('gulp-rev'),
-    revReplace = require('gulp-rev-replace'),
-    replace = require('gulp-replace'),
-    filter = require('gulp-filter'),
-    through = require('through2'),
-    fs = require('fs'),
-    manifest = {}
+var gulp          = require('gulp'),
+    ossup         = require('gulp-oss-up'),
+    newer         = require('gulp-newer'),
+    useref        = require('gulp-useref'),
+    del           = require('del'),
+    runSequence   = require('run-sequence'),
+    rev           = require('gulp-rev'),
+    revReplace    = require('gulp-rev-replace'),
+    replace       = require('gulp-replace'),
+    filter        = require('gulp-filter')
 
-// cdn options
-var cdn_host = process.env.NODE_ENV == 'production' ? process.env.CDN_HOST : ''
-// aliyun options
-var aliyunOptions = {
-  'accessKeyId': process.env.ACCESS_KEY_ID,
-  'accessKeySecret': process.env.ACCESS_KEY_SECRET,
-  'bucket': process.env.BUCKET
-}
+var putalioss     = require('./my-gulp-plugin/gulp-putalioss')
+    
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+
+var cdnHost = process.env.CDN_HOST || '',
+    aliyunOptions = {
+      'accessKeyId': process.env.ACCESS_KEY_ID || '',
+      'accessKeySecret': process.env.ACCESS_KEY_SECRET || '',
+      'bucket': process.env.BUCKET || '',
+      'region': process.env.REGION || ''
+    }
 
 // clean 
-gulp.task('clean', () => {
-  return gulp.src(['dist'])
-    .pipe(clean())
+gulp.task('clean', function () {
+  return del(['dist'])
 })
 
 // build
-gulp.task('build', () => {
+gulp.task('build', function () {
   var imgFilter = filter('public/images/**/*', {restore: true});
   var fontFilter = filter('public/fonts/**/*', {restore: true});
   var jsFilter = filter('public/javascripts/doc-*.js', {restore: true});
@@ -55,104 +53,70 @@ gulp.task('build', () => {
     .pipe(jsFilter.restore)
     .pipe(revReplace({
       canonicalUris: true,
-      prefix: cdn_host
+      prefix: cdnHost
     }))
     .pipe(gulp.dest('dist'))
 })
 
 // upload files to aliyun
-gulp.task('init-manifest', () => {
-  var content = fs.readFileSync('./manifest.json', {flag: 'a+'}).toString()
-  manifest = JSON.parse(content || '{}');
-  return gulp
-})
 
-gulp.task('oss-css', () => {
+gulp.task('oss-css', function () {
   var cssOptions = cloneObj(aliyunOptions)
   cssOptions.objectDir = 'stylesheets'
   return gulp.src('dist/stylesheets/doc-*.css')
-    .pipe(checkManifest('stylesheets'))
-    .pipe(ossup(cssOptions))
-    .pipe(gulp.dest('dist-bak/stylesheets/'))
+    .pipe(putalioss(cssOptions))
 })
 
-gulp.task('oss-js', () => {
+gulp.task('oss-js', function () {
   var jsOptions = cloneObj(aliyunOptions)
   jsOptions.objectDir = 'javascripts'
   return gulp.src('dist/javascripts/doc-*.js')
-    .pipe(checkManifest('javascripts'))
-    .pipe(ossup(jsOptions))
-    .pipe(gulp.dest('dist-bak/javascripts/'))
+    .pipe(putalioss(jsOptions))
 })
 
-gulp.task('oss-img', () => {
+gulp.task('oss-img', function () {
   var imgOptions = cloneObj(aliyunOptions)
   imgOptions.objectDir = 'images'
   return gulp.src('dist/images/**/*')
-    .pipe(checkManifest('images'))
-    .pipe(ossup(imgOptions))
-    .pipe(gulp.dest('dist-bak/images/'))
+    .pipe(putalioss(imgOptions))
 })
 
-gulp.task('oss-font', () => {
+gulp.task('oss-font', function () {
   var fontOptions = cloneObj(aliyunOptions)
   fontOptions.objectDir = 'fonts'
   return gulp.src('dist/fonts/**/*')
-    .pipe(checkManifest('fonts'))
-    .pipe(ossup(fontOptions))
-    .pipe(gulp.dest('dist-bak/fonts/'))
+    .pipe(putalioss(fontOptions))
 })
 
-gulp.task('update-manifest', () => {
-  fs.writeFileSync('manifest.json', JSON.stringify(manifest))
-  return gulp
+gulp.task('clean-assets', function () {
+  return del([
+    'dist/images',
+    'dist/fonts',
+    'dist/javascripts',
+    'dist/stylesheets'])
 })
 
-gulp.task('clean-assets', () => {
-  return gulp.src([
-      'dist/images',
-      'dist/fonts',
-      'dist/javascripts',
-      'dist/stylesheets'])
-    .pipe(clean())
-})
-
-gulp.on('stop', () => { process.exit(0) })
-
-// utils
-var cloneObj = (obj) => JSON.parse(JSON.stringify(obj))
-
-var checkManifest = (type) => {
-  return through.obj((chunk, enc, cb) => {
-    manifest[type] || (manifest[type] = [])
-    if (manifest[type].indexOf(chunk.path) > -1) {
-      cb(null, null)
-    } else {
-      manifest[type].push(chunk.path)
-      cb(null, chunk)
-    }
-  })
+if (!development()) {
+  gulp.on('stop', function () { process.exit(0) })
 }
 
-gulp.task('default', (cb) => {
-  var tasks = []
-  switch (process.env.NODE_ENV) {
-    case 'production':
-      tasks = [
-        'clean',
-        'build',
-        'init-manifest',
-        ['oss-js', 'oss-css', 'oss-font', 'oss-img'],
-        'update-manifest',
-        'clean-assets',
-        cb
-      ]
-      break
-    default:
-      tasks = [
-        'clean',
-        'build'
-      ]
+// helpers
+function cloneObj (obj) {
+  return JSON.parse(JSON.stringify(obj))
+}
+
+function development () {
+  return process.env.NODE_ENV === 'development'
+}
+
+gulp.task('default', function (cb) {
+  if (!development()) {
+    runSequence(
+      'clean',
+      'build',
+      ['oss-js', 'oss-css', 'oss-font', 'oss-img'],
+      'clean-assets',
+      cb
+    )
   }
-  runSequence.apply(this, tasks)
 });
