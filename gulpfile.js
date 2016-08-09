4c82b6c98c21701c1ec79e882016d99e6f44c297
qingwen.yan <qingwen.yan@dji.com>
2016-08-09 10:46:44 +0800

    update beta

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,12 @@ var gulp          = require('gulp'),
     rev           = require('gulp-rev'),
     revReplace    = require('gulp-rev-replace'),
     replace       = require('gulp-replace'),
-    filter        = require('gulp-filter')
-
-var putalioss     = require('./my-gulp-plugin/gulp-putalioss')
+    filter        = require('gulp-filter'),
+    through2      = require('through2'),
+    AliOSS        = require('ali-oss').Wrapper,
+    path          = require('path'),
+    fs            = require('fs'),
+    gutil         = require('gulp-util')
     
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
@@ -117,4 +120,36 @@ gulp.task('default', function (cb) {
       cb
     )
   }
-});
+})
+
+
+// oss helper
+var putalioss = through2.obj(function (chunk, enc, cb) {
+  var currentDir = process.cwd(),
+      prefix = options.prefix || '',
+      manifestPath = path.join(currentDir, options.manifestPath || '.tmp'),
+      manifestFile = path.join(manifestPath, prefix + '-manifest.json'),
+      client = new AliOSS(options)
+
+  fs.existsSync(manifestPath) || fs.mkdirSync(manifestPath)
+  var manifest = JSON.parse(fs.readFileSync(manifestFile, {flag: 'a+'}).toString() || '[]')
+
+  if (chunk.isBuffer()) {
+    var name = path.join(prefix, chunk.path.replace(chunk.base, ''))
+    if (manifest.indexOf(name) > -1) {
+      cb(null, chunk)
+    } else {
+      client.put(name, chunk.path).then(function (val) {
+        gutil.log(gutil.colors.green('[OK]'), val.name)
+        manifest.push(val.name)
+        fs.writeFileSync(manifestFile, JSON.stringify(manifest))
+        cb(null, chunk)
+      }).catch (function (err) {
+        gutil.log(gutil.colors.red('[Err]'), err)
+        cb(null, chunk)
+      });
+    }
+  } else {
+    cb(null, chunk)
+  }
+})
